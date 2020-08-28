1b93f8ba2ab169815845e5ea6760162a3dfa1ac7
Alejandro <alejandro.devop@gmail.com>
2020-08-27 19:36:14 -0500

    Added constant files

---
+++ constants.js
@@ -0,0 +1,40 @@
+export const autoCompleteInputTypes = [
+  'off',
+  'username',
+  'password',
+  'email',
+  'name',
+  'tel',
+  'street-address',
+  'postal-code',
+  'cc-number',
+  'cc-csc',
+  'cc-exp',
+  'cc-exp-month',
+  'cc-exp-year',
+];
+
+export const keyboardTypes = [
+  'default',
+  'email-address',
+  'numeric',
+  'phone-pad',
+  'ascii-capable',
+  'numbers-and-punctuation',
+  'url',
+  'number-pad',
+  'name-phone-pad',
+  'decimal-pad',
+  'twitter',
+  'web-search',
+  'visible-password',
+];
+
+export const autoCapitalizeTypes = ['sentences', 'characters', 'words', 'none'];
+
+export const userStatuses = {
+  ACTIVE: 1,
+  IN_REGISTER: 4,
+};
+
+export const REQUIRED_DOCUMENTATION_PENDING_STATUS = 18;
